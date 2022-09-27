bd7d94fd161115305b6fed597b66e692385579dd
lauragreemko <laura.barreda@greemko.com>
2022-09-27 14:18:39 +0200

    removed run

--- app.py
+++ app.py
@@ -27,5 +27,3 @@ def hello():
 #     else:
 #         prediction = model.predict([[tv,radio,newspaper]])
 #         return "The prediction of sales investing that amount of money in TV, radio and newspaper is: " + str(round(prediction[0],2)) + 'k €'
-
-app.run()
